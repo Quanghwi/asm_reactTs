799928d088e623c250041f0b4d3516c26be3bd86
Quanghwi <huydqph24532@fpt.edu.vn>
2023-04-05 07:55:20 +0700

    router - dbjson

---
+++ React_Ts/src/api/product.tsx
@@ -0,0 +1,20 @@
+import { IProduct } from "../interface/interface"
+import { instance } from "./instance"
+
+const getAll = () => {
+    return instance.get('/products')
+}
+const getOne = (id: number) => {
+    return instance.get('/products/' + id)
+}
+const addProduct = (product:IProduct) => {
+    return instance.post('/products/', product)
+}
+const deleteProduct = (id: number) => {
+    return instance.delete('/products/' + id)
+}
+const updateProduct = (product:IProduct) => {
+    return instance.put('/products/' + product.id, product)
+}
+
+export {getAll,getOne,addProduct,deleteProduct,updateProduct}
---
+++ React_Ts/src/page/layout/WebLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+const WebLayout = () => {
+  return (
+    <div><h1>ứv</h1>
+        <Outlet />
+    </div>
+  )
+}
+
+export default WebLayout
